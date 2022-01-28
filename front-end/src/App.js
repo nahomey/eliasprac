b593ecabe8e44620c0f1a33ab727211412ab8242
nahomey <nahiob2@gmail.com>
2022-01-28 13:32:49 -0600

    form is ready

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import Button from "@mui/material/Button";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App()
+{
+  return (<div className="App" >
+
+    <h1>Log in form</h1>
+    <label>first name</label>
+    <input type="text" placeholder="enter name" />
+    <label>last name</label>
+    <input type="text" placeholder="enter name" />
+    <label>email</label>
+    <input type="text" placeholder="enter email" />
+    <label>age</label>
+    <input type="number" placeholder="enter number" />
+    <Button variant="button">submit</Button>
+
+
+
+
+  </div>
   );
 }
 
